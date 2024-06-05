69f9bb5fc905007a9ea2090a25e55deb13ea44e7
Peebam <baptiste@rvrn.fr>
2024-06-05 22:07:21 +0200

    chore:setup project

--- scenes/ui/hud.gd
+++ scenes/ui/hud.gd
@@ -1,9 +1,1 @@
 extends Control
-
-# Signals
-
-func _on_run_stop_button_pressed() -> void:
-	var event = InputEventAction.new()
-	event.action = "ui_cancel"
-	event.pressed = true
-	Input.parse_input_event(event)
